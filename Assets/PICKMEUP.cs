2c728c25b313185c70ea1dfe41e280b0dc32ef91
miimxsa <ACARABA004@CITYMAIL.CUNY.EDU>
2024-12-10 22:22:11 -0500

    YEA

--- Assets/PICKMEUP.cs
+++ Assets/PICKMEUP.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;  
 
-public class PICKMEUP : MonoBehaviour
+public class NotebookScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2 minSpawnPos;   
+    [SerializeField] private Vector2 maxSpawnPos;   
+
+    [SerializeField] private GameObject notebook;    
+    private bool isPickedUp = false;  
+    private const string notebookPickedUpKey = "NotebookPickedUp";  
+
+    private PlayerInput playerInput;  
+
+    private void Awake()
+    {
+        
+        playerInput = FindObjectOfType<PlayerInput>();  
+    }
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt(notebookPickedUpKey, 0) == 1)
+        {
+            notebook.SetActive(false); 
+        }
+        else
+        {
+            SpawnNotebookAtRandomLocation();
+        }
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnNotebookAtRandomLocation()
     {
-        
+        float randomX = Random.Range(minSpawnPos.x, maxSpawnPos.x);
+        float randomY = Random.Range(minSpawnPos.y, maxSpawnPos.y);
+        Vector2 randomPosition = new Vector2(randomX, randomY);
+
+        notebook.transform.position = randomPosition;
+
+        notebook.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (playerInput.actions["Interact"].triggered)  
+            {
+                isPickedUp = true;
+
+                notebook.SetActive(false);
+
+                PlayerPrefs.SetInt(notebookPickedUpKey, 1); 
+                PlayerPrefs.Save();  
+            }
+        }
     }
 }
